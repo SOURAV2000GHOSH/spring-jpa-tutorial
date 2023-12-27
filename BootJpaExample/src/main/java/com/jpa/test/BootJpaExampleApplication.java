b2cf135c25e895b4d038495c2029bec65a51da7c
Sourav <souravghosh6543@gmail.com>
2023-12-27 11:25:15 +0530

    uploading full spring-jpa-tutorial project

---
+++ BootJpaExample/src/main/java/com/jpa/test/BootJpaExampleApplication.java
@@ -0,0 +1,34 @@
+package com.jpa.test;
+
+import java.util.List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.jpa.test.Entities.User;
+import com.jpa.test.dao.UserRepository;
+
+@SpringBootApplication
+public class BootJpaExampleApplication {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(BootJpaExampleApplication.class, args);
+		
+		UserRepository repository= context.getBean(UserRepository.class);
+		
+//		User user=new User();
+//		user.setName("Sourav Ghosh");
+//		user.setCity("Kharagpur");
+//		user.setStatus("I am a java programmer");
+//		
+//		User user2 = repository.save(user);
+//		
+//		System.out.println(user2);
+		
+		//example of @Query
+		List<User> userByName = repository.getUserByName("Sourav Ghosh");
+		userByName.forEach(user -> System.out.println(user));
+	}
+
+}
---
+++ BootJpaExample/src/main/java/com/jpa/test/dao/UserRepository.java
@@ -0,0 +1,27 @@
+package com.jpa.test.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.jpa.test.Entities.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+	public List<User> findByName(String name);
+	
+	public List<User> findByNameAndCity(String name, String city);
+	
+	// Java Persistence Query
+	@Query("select u from user u")
+	public List<User> getAllUser();
+	
+	@Query("select u from user u where u.name=?1")
+	public List<User> getUserByName(String name);
+	
+	// sql native query / sql normal query adding formats
+	@Query(value = "Select * from user", nativeQuery = true)
+	public List<User> getUsers();
+}
